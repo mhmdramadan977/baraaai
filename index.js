d3d2f9f7cf03e09fdbc289ff883f91ee65f9a95a
mhmdramadan977 <mr19mr11mr97@gmail.com>
2024-11-02 20:30:33 +0300

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(
+    //__dirname,
+     './client/build')));
 
 
 app.use(cors());
@@ -66,7 +68,9 @@ app.put('/api/orders/:id', (req, res) => {
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(
+        //__dirname,
+         './client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
